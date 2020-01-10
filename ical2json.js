480ebc52c024952f0c8c121f457057298fd5f567
wlam-git <5207winlam@gmail.com>
2020-01-10 12:23:53 +1100

    added converter function

--- ical2json.js
+++ ical2json.js
@@ -1,16 +1,24 @@
 var ical2json = require('ical2json');
 var fs = require('fs');
 
-// Read in ical file
-var input = fs.readFileSync('exampleEvents.ics', 'utf8');
+function converter(ics_filename) {
+    // Read in ical file
+    var input = fs.readFileSync(ics_filename, 'utf8');
 
-// Convert ical to json
-var output = ical2json.convert(input);
+    // Convert ical to json
+    var output = ical2json.convert(input);
 
-// Output file to json format
-fs.writeFile('calendar.json', JSON.stringify(output), (err) => { 
-      
-    // In case of a error throw err. 
-    if (err) throw err;
-})
+    // Output file to json format
+    fs.writeFile('calendar.json', JSON.stringify(output), (err) => { 
+        
+        // In case of a error throw err. 
+        if (err) throw err;
+    return;
+    }) 
+}
+
+converter('exampleEvents.ics');
+
+
+
 
