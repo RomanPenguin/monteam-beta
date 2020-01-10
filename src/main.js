5d125bf2949b0516e6a6b581a5a0d3fe60891e52
RomanPenguin <49329186+RomanPenguin@users.noreply.github.com>
2020-01-10 14:07:28 +1100

    what
    Creating aws something something

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 // import App from './App.vue'
-// import Header from './components/Header.vue'
-// import Event from './components/Event.vue'
-import Calendar from './components/Calendar.vue'
-import VCalendar from 'v-calendar';
-
+import Header from './components/Header.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faSortDown, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faSortDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faBars, faSortDown, faMapMarkedAlt);
+library.add(faBars, faSortDown);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
@@ -20,16 +16,6 @@ Vue.config.productionTip = false;
 // }).$mount('#app');
 
 
-// new Vue({
-//   render: h => h(Header),
-// }).$mount('#Header');
-//
-// new Vue({
-//   render: h => h(Event),
-// }).$mount('#Event');
-
-Vue.use(VCalendar);
-
 new Vue({
-  render: h => h(Calendar),
-}).$mount('#Calendar');
+  render: h => h(Header),
+}).$mount('#Header');
